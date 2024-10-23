2c595c396a9cea3468acd6ed512357f3f9a3454f
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-23 23:28:48 +0530

    :sparkles: Started cra app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./index.css";
+
+import App from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
+  </React.StrictMode>
+);
