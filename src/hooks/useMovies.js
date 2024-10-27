928058e55edb87325fa49210f1d33149012162ca
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-27 12:08:47 +0530

    :snake: updated api key

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const KEY = "f84fc31d";
+const KEY = "5c314236";
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
